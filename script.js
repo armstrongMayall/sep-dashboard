d61fb9529ec43d0b1527c492cba173df0fca6382
iampaulio <pmhewett@gmail.com>
2025-03-22 16:32:22 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Update the header part in your SEPDashboard component
+// ...
+
+{/* Header with logo */}
+<div className="p-4" style={{ backgroundColor: colors.darkBackground }}>
+  <div className="flex items-center">
+    <div className="flex items-center">
+      <img src="assets/images/imwt-logo.png" alt="In Marketing We Trust" className="w-10 h-11" />
+      <div className="ml-3 text-white">
+        <div className="text-sm">In Marketing</div>
+        <div className="text-lg font-extrabold">We Trust</div>
+      </div>
+    </div>
+    <div className="ml-8 text-white text-lg font-extrabold">
+      Search Everywhere Performance Assessment
+    </div>
+  </div>
+</div>
+
+// ...
